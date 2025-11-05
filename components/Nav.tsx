21086217e6a1e15f23e219c804fc4b7f49ccdc40
PXXCXNT <pxxcxnt@icloud.com>
2025-11-06 01:57:30 +0900

    스크롤시 blur 효과 추가

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import styles from "./Nav.module.css";
 import ScrambleText from "./ScrambleText";
 
 export default function Nav() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className={styles.container}>
+    <nav className={`${styles.container} ${isScrolled ? styles.scrolled : ""}`}>
       <Link href="/">
         <h1>
           <ScrambleText text="PERCENTZERO" />
